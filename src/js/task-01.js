373bcd50d06624cbde72e881f40cb0fddbf48e9f
Needlife1 <needlife124@gmail.com>
2023-02-02 10:51:52 +0200

    task

---
+++ src/js/task-01.js
@@ -0,0 +1,11 @@
+const arrayOfCategories = categories.querySelectorAll(".item");
+console.log(`Number of categories: ${arrayOfCategories.length}`);
+
+const CategoriesArray = [...arrayOfCategories]
+  .map(
+    (categories) => `Category: ${categories.children[0].textContent}
+  Elements: ${categories.children[1].children.length}`
+  )
+  .join("/n");
+
+console.log(CategoriesArray);
